c8f8bb8f39753c22f3fc3e2dffca393c4ae2934a
Jarek Kubiak <jaroslawkubiak82@gmail.com>
2023-02-21 23:14:17 +0100

    first commit

--- 1 - Contains Duplicate/script.js
+++ 1 - Contains Duplicate/script.js
@@ -1,6 +1,4 @@
 'use strict';
-console.log('Contains Duplicate');
-// TERMS
 // Given an integer array nums, return true if any value appears at least twice in the array,
 // and return false if every element is distinct.
 
