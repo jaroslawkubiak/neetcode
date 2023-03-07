bf7d6b0dd282adf7dc7dc2fb3d8e148353d7fcbb
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-03-07 16:16:50 +0100

    trapping water

--- Two Pointers/5 - trapping rain water/script.js
+++ Two Pointers/5 - trapping rain water/script.js
@@ -1,5 +1,40 @@
 "use strict";
 
-const nums = [4, 2, 0, 3, 2, 5];
+const nums = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
 
-var trap = function (height) {};
+Array.prototype.max = function () {
+  return Math.max.apply(null, this);
+};
+
+var trap = function (height) {
+  const maxLeft = [];
+  const maxRight = [];
+  const min_value = [];
+  let res = 0;
+
+  for (let i = 0; i < height.length; i++) {
+    let maxL = 0;
+    let maxR = 0;
+
+    // according to neetcode this code don't work properly - it take too much time
+    // so dooing 2x for loop is better than using slice method
+    maxL = height.slice(0, i);
+    maxR = height.slice(i + 1, height.length);
+
+    if (maxL.length) {
+      maxL = maxL.max();
+      maxLeft.push(maxL);
+    } else maxLeft.push(0);
+
+    if (maxR.length) {
+      maxR = maxR.max();
+      maxRight.push(maxR);
+    } else maxRight.push(0);
+
+    min_value.push(Math.min(maxLeft[i], maxRight[i]));
+    if (min_value[i] - height[i] > 0) res += min_value[i] - height[i];
+  }
+  return res;
+};
+
+console.log(trap(nums));
